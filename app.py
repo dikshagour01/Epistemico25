2ec6a1fd773343e9e9142bb4842f244daa89c853
Gargi <98906186+GargiMittal@users.noreply.github.com>
2025-03-10 21:50:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,12 @@
-import cloudinary
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory, make_response
 from config import Config
 from models import db
 from flask_migrate import Migrate
 from routes import app_routes
 from admin import setup_admin
 
-
 app = Flask(__name__)
 app.config.from_object(Config)
-
 
 # Initialize Database
 db.init_app(app)
@@ -22,8 +19,16 @@ app.register_blueprint(app_routes)
 def gallery():
     return render_template("gallery.html")
 
+
+# Serve static files with cache control
+@app.route('/static/<path:filename>')
+def cached_static(filename):
+    response = make_response(send_from_directory('static', filename))
+    response.headers['Cache-Control'] = 'public, max-age=31536000'  # Cache for 1 year
+    return response
+
 # Setup Admin Panel
 setup_admin(app)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=10000) 
+    app.run(debug=True)
